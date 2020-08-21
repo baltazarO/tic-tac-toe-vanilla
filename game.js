3e78d35947fb999a05b3ecadcce714a42c9c2053
Baltazar Ortiz <bortiz19@mail.wou.edu>
2020-08-20 21:35:43 -0700

    Full games with some bugs

--- game.js
+++ game.js
@@ -1,37 +1,53 @@
-var button = document.querySelector("button");
-button.onclick = restart;
-var allSquares = document.getElementsByTagName("td");
-var isPlayerTurn = document.querySelector('input:checked').value;
-var computersIcon = (isPlayerTurn) ? "clicked-circle" : "clicked-x";
-var playersIcon = (isPlayerTurn) ? "clicked-x" : "clicked-circle";
+const button = document.querySelector("button");
+const allSquares = document.getElementsByTagName("td");
+var isPlayerTurn = document.querySelector('input').value;
+const computersIcon = (isPlayerTurn) ? "clicked-circle" : "clicked-x";
+const playersIcon = (isPlayerTurn) ? "clicked-x" : "clicked-circle";
+var gameOver = false;
+const WINNING_COMBOS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
+button.onclick = restart;
 setUpBoard();
 
 function restart() {    //get all squares and make them all display none
+    gameOver = false;
     for (let i = 0; i < allSquares.length; i++) {
         allSquares[i].classList.remove("clicked-x");
         allSquares[i].classList.remove("clicked-circle");
     }
 }
 
-function yourTurn(btn) {   //play game
-    computerTurn();
-    if (isPlayerTurn) {
-        if (!btn.classList.contains("clicked-x") && !btn.classList.contains("clicked-circle")) {
-            btn.classList.add(playersIcon);
-            isPlayerTurn = false;
+function squareClick(btn) {   //play game
+    if (!gameOver) {
+        if (isPlayerTurn) {
+            if (!btn.classList.contains("clicked-x") && !btn.classList.contains("clicked-circle")) {
+                btn.classList.add(playersIcon); // add my icon to square visually
+                isPlayerTurn = false;
+            }
         }
+        checkWin(playersIcon);
+        computerTurn();
     }
 }
 
 function computerTurn() {
     if (!isPlayerTurn) {    //pick random square
         let emptyIndex = Math.floor(Math.random() * 9);
-        while(spaceOccupied(emptyIndex)) {
+        while (spaceOccupied(emptyIndex)) {
             emptyIndex = Math.floor(Math.random() * 9);
         }
         allSquares[emptyIndex].classList.add(computersIcon);
         isPlayerTurn = true;
+        checkWin(computersIcon);
     }
 }
 
@@ -42,9 +58,37 @@ function spaceOccupied(index) {
 }
 
 function setUpBoard() {
-    for(let x = 0; x < allSquares.length; x++) {
-        allSquares[x].addEventListener("click",function() {
-            yourTurn(allSquares[x]);
+    for (let x = 0; x < allSquares.length; x++) {
+        allSquares[x].addEventListener("click", function () {
+            squareClick(allSquares[x]);
         });
     }
+    computerTurn();
+}
+
+function checkWin(side) {
+    if (checkWinCombo(side)) {
+        gameOver = true;
+        alert(side + " wins the game!!!");
+    } else if (checkDraw()) {
+        gameOver = true;
+        alert("Game is a draw");
+    }
+}
+
+function checkWinCombo(side) {
+    return WINNING_COMBOS.some(combo => {
+        return combo.every(square => {
+            return allSquares[square].classList.contains(side);
+        })
+    })
+}
+
+function checkDraw() {
+    for (let index = 0; index < allSquares.length; index++) {
+        if (!allSquares[index].classList.contains("clicked-x") && !allSquares[index].classList.contains("clicked-circle")) {
+            return false;
+        }
+    }
+    return true;
 }
